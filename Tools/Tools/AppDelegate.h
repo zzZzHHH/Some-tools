7c3f42b6034bda627322cfb3ec972bf6cb31339f
zlh <zlh@qq.com>
2016-07-21 15:00:51 +0800

    初始化项目

---
+++ Tools/Tools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tools
+//
+//  Created by 周立贺 on 16/7/21.
+//  Copyright © 2016年 周立贺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tools/Tools/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tools
+//
+//  Created by 周立贺 on 16/7/21.
+//  Copyright © 2016年 周立贺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
